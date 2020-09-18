9a08b362759e09bcac25990c2bf3858e451626ca
LaoPeng <2779824672@qq.com>
2020-09-18 14:54:19 +0800

    暂存区到本地分支

---
+++ src/org/pjj/entity/Student.java
@@ -0,0 +1,14 @@
+package org.pjj.entity;
+
+public class Student{
+	private int id;
+	private String name;
+	private int age;
+
+	public Student(){
+	}
+	
+	public void test(){
+		System.out.println(id+"-"+name+"-"+age);
+	}
+}
